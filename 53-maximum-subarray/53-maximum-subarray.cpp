477e9d99282434b4403d8686d9ef045fb9000b46
Twiggz#78800 <31563180+TwiggiestOak18@users.noreply.github.com>
2022-01-15 16:31:02 +0530

    Time: 243 ms (5.14%), Space: 67.6 MB (91.49%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> sub_arr;
+       
         int s =0;
         int msf = nums[0];
         int meh= 0;
@@ -10,11 +10,7 @@ public:
             meh+=nums[i];
             if(meh > msf)
             {
-//sub_arr.clear();
                 msf= meh;
-              // sub_arr.push_back(s);
-             //   sub_arr.push_back(i);
-                //
             }
             
             if(meh<0)
